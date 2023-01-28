a5205c7945ae95246940e1e82529c82c1e79030d
Michael Jurkoic <mjjurkoic@gmail.com>
2023-01-28 00:11:28 -0600

    Prep for argument parsing

---
+++ src/arg_handler.py
@@ -0,0 +1,7 @@
+import argparse
+
+def parse_runtime_args():
+    parser = argparse.ArgumentParser(
+        prog = "Git-Crunchin",
+        description = "Generate beautiful transcripts of the pod and the freaking cast.",
+    )
