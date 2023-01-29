9f7e6b7fac531eef3da1f8587b172572e665923c
Michael Jurkoic <mjjurkoic@gmail.com>
2023-01-29 13:20:27 -0600

    Enable parallel transcription of multiple files

--- src/scribe/transcriber.py
+++ src/scribe/transcriber.py
@@ -1,7 +1,12 @@
+from collections.abc import Callable
+from multiprocessing import Pool
+from multiprocessing.pool import AsyncResult
 from typing import List
 import whisper
 
 
+# TODO: Add error handling.
+# TODO: Add per-file progress tracking.
 class Transcriber:
     model = None
     completed_files: List[str] = []
@@ -11,7 +16,37 @@ class Transcriber:
         self.model = whisper.load_model(whisper_model)
 
     def transcribe(self, filename: str) -> str:
+        """Transcribe a single audio file from the given filename.
+
+        Args:
+            filename (str): The path to the audio file.
+
+        Returns:
+            str: The full transcribed text.
+        """
         self.requested_files.append(filename)
         transcription = self.model.transcribe(filename)
         self.completed_files.append(filename)
         return transcription
+
+    def transcribe_many(
+        self,
+        filenames: List[str],
+        callback: Callable[[str], None],
+        num_processes: int = 4
+    ) -> AsyncResult:
+        """Transcribes many audio files in parallel, passing each transcription to the callback
+        function as it finishes.
+
+        Args:
+            filenames (List[str]): A list of filepaths to audio files.
+            callback (Callable[[str], None]): The function to be executed when a transcription is
+            finished. Should accept a single string containing the full transcript.
+            num_processes (int, optional): The max number of processes to run simultaneously.
+            Defaults to 4.
+
+        Returns:
+            AsyncResult: Indicates when all the input files have been transcribed.
+        """
+        with Pool(num_processes) as pool:
+            return pool.apply_async(self.transcribe, args = filenames, callback = callback)
