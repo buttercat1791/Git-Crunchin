96a03f88a6dc441407ebb46fb7e8d997bc0009b0
Michael Jurkoic <mjjurkoic@gmail.com>
2023-02-25 17:09:05 -0600

    Bug fixes

--- src/editor/redactor.py
+++ src/editor/redactor.py
@@ -10,11 +10,11 @@ class Redactor:
         ('pilgrimage_start', 'pilgrimage_end'),
     ]
 
-    ad_regexes: dict[str, Pattern[AnyStr@compile]] = {
+    ad_regexes: dict[str, Pattern] = {
         'aquinas_wealth_start': re.compile(r'(?i)this\s+episode\s+is\s+sponsored\s+by\s+\bAquinas\s+Wealth\s+Advisors\b'),
         'aquinas_wealth_end': re.compile(r'(?i)Aquinas\s+Wealth\s+Advisors\s+a\s+registered\s+investment\s+advisor\s+with\s+the\s+Securities\s+and\s+Exchange\s+Commission[.]?\s+'),
         'pilgrimage_start': re.compile(r'(?i)hey\s+guys[.,;]?\s+Patrick\s+here\s+with\s+a\s+pretty\s+exciting\s+announcement\s+'),
-        'pilgirmage_end': re.compile(r'(?i)now[.,;]?\s+back\s+to\s+the\s+episode\s+[,;]?guys[.,;]?\s+yeah[.]?\s+'),
+        'pilgrimage_end': re.compile(r'(?i)now[.,;]?\s+back\s+to\s+the\s+episode\s+[,;]?guys[.,;]?\s+yeah[.]?\s+'),
     }
 
     def remove_ads(self, transcript: str) -> str:
@@ -29,8 +29,8 @@ class Redactor:
         processed_text = transcript
 
         for ad in self.ad_list:
-            start_match: Match[str] = self.ad_regexes[ad[0]].search(transcript)
-            end_match: Match[str] = self.ad_regexes[ad[1]].search(transcript)
+            start_match: Match = self.ad_regexes[ad[0]].search(processed_text)
+            end_match: Match = self.ad_regexes[ad[1]].search(processed_text)
 
             if start_match and end_match:
                 processed_text = processed_text[:start_match.start()] + processed_text[end_match.end():]
